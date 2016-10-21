ba836ebf30a250af650142ab844f066bf3cf34bc
wonwoo <aoruqjfu@gmail.com>
2016-10-21 22:45:44 +0900

    루트 요청시 message 로 리다이렉트

--- src/main/kotlin/com/example/WebConfig.kt
+++ src/main/kotlin/com/example/WebConfig.kt
@@ -7,5 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 open class WebConfig : WebMvcConfigurerAdapter(){
 
-    override fun addViewControllers(registry: ViewControllerRegistry) = registry.addViewController("/login").setViewName("login")
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/login").setViewName("login")
+        registry.addViewController("/").setViewName("redirect:/message")
+    }
 }
