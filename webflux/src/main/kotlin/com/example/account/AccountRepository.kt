a11bc55d14043f4d232cd86fce9f79d575d61503
wonwoo <aoruqjfu@gmail.com>
2019-09-22 17:09:02 +0900

    type 수정

--- webflux/src/main/kotlin/com/example/account/AccountRepository.kt
+++ webflux/src/main/kotlin/com/example/account/AccountRepository.kt
@@ -3,7 +3,7 @@ package com.example.account
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import reactor.core.publisher.Mono
 
-interface AccountRepository : ReactiveMongoRepository<Account, Long> {
+interface AccountRepository : ReactiveMongoRepository<Account, String> {
 
     fun findByname(name: String): Mono<Account>
 
