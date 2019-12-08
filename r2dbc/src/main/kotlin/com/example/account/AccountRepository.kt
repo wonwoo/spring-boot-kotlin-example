c2e5419ba53730028b5dcbd87e3778e1b6fd92fc
wonwoo <aoruqjfu@gmail.com>
2019-12-08 12:23:02 +0900

    spring boot version upgrade

--- r2dbc/src/main/kotlin/com/example/account/AccountRepository.kt
+++ r2dbc/src/main/kotlin/com/example/account/AccountRepository.kt
@@ -1,6 +1,6 @@
 package com.example.account
 
-import org.springframework.data.r2dbc.repository.query.Query
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Mono
 
