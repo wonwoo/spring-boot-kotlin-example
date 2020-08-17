73b972aa1ff95cb40b0476d0c72c8ab48f42bd98
wonwoo <aoruqjfu@gmail.com>
2020-08-17 22:25:23 +0900

    version upgrade

--- r2dbc/src/main/kotlin/com/example/InitializerSchema.kt
+++ r2dbc/src/main/kotlin/com/example/InitializerSchema.kt
@@ -1,21 +1,22 @@
 package com.example
 
 import io.r2dbc.spi.ConnectionFactory
-import org.springframework.boot.ApplicationArguments
-import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
 import org.springframework.core.annotation.Order
-import org.springframework.core.io.ResourceLoader
-import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 import org.springframework.stereotype.Component
 
 @Component
 @Order(10)
-class InitializerSchema(private val connectionFactory: ConnectionFactory,
-                        private val resourceLoader: ResourceLoader) : ApplicationRunner {
+class InitializerSchema {
 
-    override fun run(args: ApplicationArguments) {
-        val scripts = resourceLoader.getResource("classpath:schema.sql")
-        ResourceDatabasePopulator(scripts).execute(connectionFactory).block()
+    @Bean
+    fun connectionFactoryInitializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
+        return ConnectionFactoryInitializer().apply {
+            this.setConnectionFactory(connectionFactory)
+            this.setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
+        }
     }
-
 }
